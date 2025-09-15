5149cb796875f61b1856ad47cef3ca2f27aacb51
yangclima <yangclima@gmail.com>
2025-09-15 06:53:34 -0300

    feat: add `/api/v1/status/` endpoint

---
+++ src/test/api/v1/status/get.test.js
@@ -0,0 +1,13 @@
+test('GET to /api/v1/status', async () => {
+  const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(200);
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parseUpdatedAt).toEqual(responseBody.updated_at);
+
+  expect(responseBody.dependencies.database.version).toEqual('17.6');
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+});
