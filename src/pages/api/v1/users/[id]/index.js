0aa6bacac380692716c36c947843d481aa2a7287
yangclima <yangclima@gmail.com>
2025-09-22 01:46:49 -0300

    feat: add PATCH `api/v1/users/:id` endpoint

--- src/pages/api/v1/users/[id]/index.js
+++ src/pages/api/v1/users/[id]/index.js
@@ -2,6 +2,7 @@ import { createRouter } from 'next-connect';
 
 import user from 'models/user';
 import controller from 'infra/controller';
+import { BadRequestError } from 'infra/errors';
 
 async function getHandler(req, res) {
   const { id } = req.query;
@@ -16,8 +17,31 @@ async function getHandler(req, res) {
   res.status(status).json(response);
 }
 
+async function patchHandler(req, res) {
+  const { id } = req.query;
+
+  const { email, username, password } = req.body;
+
+  if (!email && !username && !password) {
+    throw new BadRequestError({
+      message:
+        'Pelomenos um dos seguintes campos deve ser fornecido: username, password ou email',
+      action: 'verifique os dados enviados e tente novamente',
+    });
+  }
+
+  const updatedUser = await user.update(id, req.body);
+  const { password: _, ...safeUser } = updatedUser;
+
+  const status = 201;
+  const response = safeUser;
+
+  res.status(status).json(response);
+}
+
 const usersIdRouter = createRouter();
 
 usersIdRouter.get(getHandler);
+usersIdRouter.patch(patchHandler);
 
 export default usersIdRouter.handler(controller.errorHandler);
