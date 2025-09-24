b857a9e59c13fa345ed9f574ba024dd5603dc829
yangclima <yangclima@gmail.com>
2025-09-24 20:30:25 -0300

    build: change  config in  infra module to false

--- src/infra/email.js
+++ src/infra/email.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
-  secure: process.env.NODE_ENV === 'production',
+  secure: false,
   auth: {
     user: process.env.EMAIL_SMTP_USER,
     pass: process.env.EMAIL_SMTP_PASSWORD,
